9f75ac2776e7fb96a8feda07b76b8990c0400180
unknown <eimanhamdy99@gmail.com>
2025-06-24 19:12:06 -0700

    Add routing for multiple pages (home, cart, login, register)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Products } from './products/products'; 
+import { Cart } from './cart/cart'; 
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Products},
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'cart', component: Cart },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Products } from './products/products';
 import { Navbar } from './navbar/navbar';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Cart } from './cart/cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Products,NgbModule],
+  imports: [RouterOutlet,Navbar,Products,NgbModule,Cart],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
