e777e1ad338647017547a45bf0a777f8290d6e26
unknown <eimanhamdy99@gmail.com>
2025-06-25 21:09:13 -0700

    Render login and register pages

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule,NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
-export class Login {
-
+export class Login{
+  submit(loginForm:NgForm){
+    console.log(loginForm.value)
+  }
 }
--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule,NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
+registerForm!: FormGroup;
 
+ngOnInit(){
+  this.registerForm = new FormGroup({
+  name: new FormControl("", [Validators.required]),
+  email: new FormControl("", [
+    Validators.required,
+    Validators.pattern("^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+  ]),
+  username: new FormControl("", [Validators.required, Validators.pattern(/^\S+$/)]),
+  password: new FormControl("", [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&%#]).+$/)])
+});
+
+}
+
+submit(){
+  console.log(this.registerForm.value)
+}
 }
